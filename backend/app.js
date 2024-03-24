49b3113cd6d0d0032c99e0f8ee164e2082d6177e
aerraj <rraj09102002@gmail.com>
2024-03-25 02:08:01 +0530

    first-commit

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import messageRouter from "./router/messageRouter.js";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+const app = express();
+
+dotenv.config({path:"./config/config.env"});
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ["POST"],
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/api/v1/message',messageRouter);
+
+
+dbConnection();
+
+export default app;
+
