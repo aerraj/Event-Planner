829530f4e5472e944655b652121df1ef67aac984
aerraj <rraj09102002@gmail.com>
2024-03-25 02:44:56 +0530

    second-backend-commit

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,13 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.get('/',(req,res)=>{
+    res.json({
+        success:true,
+        message:"Welcome to Lucknow"
+    })
+})
+
 app.use('/api/v1/message',messageRouter);
 
 
